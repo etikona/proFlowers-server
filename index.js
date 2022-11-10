5385e27f5e70638709a7051d7eab4fb89aa9cff5
eti kona <etikonapal@gmail.com>
2022-11-10 22:22:58 +0600

    Use patch

--- index.js
+++ index.js
@@ -10,23 +10,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// User : proFlowers
-// Pass: FE67qFfmJAjXKU4u
-
-
 // Add database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6hyeg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
- function verifyJWT(req, res, next){
+
+//  Verify Json web Token
+function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
-    if(!authHeader){
-        return res.status(401).send({msg: 'Unauthorized access'})
+    if (!authHeader) {
+        return res.status(401).send({ msg: 'Unauthorized access' })
     }
     const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(error, decoded){
-        if(error){
-            return res.status(403).send({msg: 'Invalid Token'})
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (error, decoded) {
+        if (error) {
+            return res.status(403).send({ msg: 'Invalid Token' })
         }
         req.decoded = decoded;
         next()
@@ -37,10 +35,10 @@ async function run() {
     try {
         const serviceCollection = client.db('proFlowers').collection('services');
         const reviewCollection = client.db('proFlowers').collection('reviews');
-        app.post('/jwt', async(req, res) => {
+        app.post('/jwt', async (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '2d'})
-            res.send({token})
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2d' })
+            res.send({ token })
         })
 
         // Send data server to client
@@ -83,6 +81,19 @@ async function run() {
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
             res.send(result);
+        })
+
+        app.patch('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const status = req.body.status;
+            const query = { _id: ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    status: status
+                }
+            }
+            const result = await reviewCollection.updateOne(query, updatedDoc);
+            res.send(result)
         })
         // Delete Review data
         app.delete('/reviews/:id', async (req, res) => {
